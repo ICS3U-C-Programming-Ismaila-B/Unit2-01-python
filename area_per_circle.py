edf045e9504a4562ad35a7eef2d64d78d9aa6e1d
Ismaila Bah <Ismaila.bah@stu.ocsb.ca>
2023-09-26 16:54:40 +0000

    “area_per-circle.py”

---
+++ area_per_circle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created by: Ismaila BAh
+# Created on: september . 26, 2023
+# This program calculates and displays the area and perimeter of a
+# circle with radius 14 mm.
+import math
+
+
+def main():
+    print("For a circle that has a radius")
+    print("of 14 mm:")
+    print("")
+    print("Area = {} mm^2".format(math.pi * 14**2))
+    print("Perimeter = {} mm".format(2 * math.pi * 14))
+
+
+if __name__ == "__main__":
+    main()
